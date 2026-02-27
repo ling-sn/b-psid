538ccb11e6434b84da7fe7c9c2367417b3674621
Sonia <lingsn@umich.edu>
2026-02-27 13:07:14 -0500

    update script so it can handle both RF and FR library types

--- II. Alignment/iv. bam_splitting/split.py
+++ II. Alignment/iv. bam_splitting/split.py
@@ -60,51 +60,75 @@ def map_forward(output: Path, small_f: int, big_F: int, bam: str):
       traceback.print_exc()
       raise
 
-def main(bam_folder: str):
+def collect_rev(rev1: Path, rev2: Path, rev: Path, bam: str):
+   """
+   PURPOSE:
+   * Collect alignments for rev.bam
+   """
+   ## 1) Second-in-pair maps to the reverse strand
+   map_reverse(rev1, 144, bam)
+
+   ## 2) First-in-pair maps to the forward strand
+   map_forward(rev2, 64, 16, bam)
+
+   ## 3) Combine alignments that originate on the reverse strand
+   merge_bams(rev1, rev2, rev)
+
+def collect_fwd(fwd1: Path, fwd2: Path, fwd: Path, bam: str):
+   """ 
+   PURPOSE:
+   * Collect alignments for fwd.bam
+   """
+   ## 1) Second-in-pair maps to the forward strand
+   map_forward(fwd1, 128, 16, bam)
+
+   ## 2) First-in-pair maps to the reverse strand
+   map_reverse(fwd2, 80, bam)
+
+   ## 3) Combine alignments that originate on the forward strand
+   merge_bams(fwd1, fwd2, fwd)
+
+def main(bam_folder: str, library_type: str):
    """
    PURPOSE:
    * Obtain specific alignments from BAM files, and then combine 
      them into separate forward and reverse strand files.
    ---
-   REQUIRES:
-   * Paired end reads with RF library type (NEBNext).
+   LIBRARY TYPE:
+   * Default: Assumes paired end reads with RF library type (NEBNext).
+   * If FR specified, then whatever is in fwd -> (goes to) rev, 
+     and whatever is in rev -> (goes to) fwd.
    """
    try:
       ## Obtain BAM file in folder
       folder = Path("realignments")/bam_folder
       bam = str(next(file for file in folder.glob("*.bam")))
 
-      """ 
-      PART I:
-      * Collect alignments for fwd.bam
+      ## Define directories
       """
-      ## 1) Second-in-pair maps to the forward strand
-      fwd1 = folder/"fwd1.bam"
-      map_forward(fwd1, 128, 16, bam)
-      
-      ## 2) First-in-pair maps to the reverse strand
-      fwd2 = folder/"fwd2.bam"
-      map_reverse(fwd2, 80, bam)
-
-      ## 3) Combine alignments that originate on the forward strand
-      fwd = folder/"fwd.bam"
-      merge_bams(fwd1, fwd2, fwd)
-
+      Explanation of directories list
+      [0] = fwd1.bam
+      [1] = fwd2.bam
+      [2] = fwd.bam
+      [3] = rev1.bam
+      [4] = rev2.bam
+      [5] = rev.bam
       """
-      PART II:
-      * Collect alignments for rev.bam
-      """
-      ## 1) Second-in-pair maps to the reverse strand
-      rev1 = folder/"rev1.bam"
-      map_reverse(rev1, 144, bam)
+      directories = []
+      for type in ["fwd", "rev"]:
+         r1 = folder/f"{type}1.bam"
+         r2 = folder/f"{type}2.bam"
+         output = folder/f"{type}.bam"
+         all_files = [r1, r2, output]
+         directories.extend(all_files)
 
-      ## 2) First-in-pair maps to the forward strand
-      rev2 = folder/"rev2.bam"
-      map_forward(rev2, 64, 16, bam)
-
-      ## 3) Combine alignments that originate on the reverse strand
-      rev = folder/"rev.bam"
-      merge_bams(rev1, rev2, rev)
+      if library_type == "RF":
+         collect_fwd(directories[0], directories[1], directories[2], bam)
+         collect_rev(directories[3], directories[4], directories[5], bam)
+      else:
+         ## Swap around output file names
+         collect_fwd(directories[3], directories[4], directories[5], bam)
+         collect_rev(directories[0], directories[1], directories[2], bam)
 
       """
       PART III:
@@ -113,8 +137,8 @@ def main(bam_folder: str):
       subprocess.run(
          [
             "rm",
-            str(fwd1), str(fwd2),
-            str(rev1), str(rev2)
+            str(directories[0]), str(directories[1]),
+            str(directories[3]), str(directories[4])
          ]
       )
 
@@ -127,9 +151,9 @@ if __name__ == "__main__":
    parser = argparse.ArgumentParser(description = ("Splits alignment BAM into separate files (forward and reverse)"
                                                    " by detecting strandedness"))
    parser.add_argument("--bam_folder", help = "Path to folder with BAM file", required = True)
-   # parser.add_argument("--library", help = "Library type. Choices: RF or FR")
+   parser.add_argument("--library_type", choices = ["RF", "FR"], default = "RF")
    args = parser.parse_args()
 
    print("Splitting BAM files...")
-   main(args.bam_folder)
+   main(args.bam_folder, args.library_type)
    print("Process finished.")
