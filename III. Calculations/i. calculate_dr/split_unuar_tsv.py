24f700c60276b45c498ffb0d1343167f495b5d21
Sonia <lingsn@umich.edu>
2026-02-27 16:31:00 -0500

    create separate script that splits UNUAR tsv once so I don't have to do it multiple times during calculate_dr

---
+++ III. Calculations/i. calculate_dr/split_unuar_tsv.py
@@ -0,0 +1,51 @@
+## Use RNA-STAR conda environment
+from pathlib import Path
+import traceback
+import argparse
+import pandas as pd
+
+## Disable .loc indexing warning
+pd.options.mode.chained_assignment = None
+
+def main():
+   try:
+      """
+      PURPOSE: 
+      1. Merges dataframes (UNUAR sites + fitted values from BID-Seq)
+      2. Splits merged dataframe by strand [+/-] column
+      """
+      current_path = Path.cwd()
+      left = pd.read_csv(Path("~/umms-RNAlabDATA/Software/B-PsiD_tools"
+                              "/UNUAR_motif_sites_mRNA_hg38p14.tsv").expanduser(), sep = "\t")
+      right = pd.read_excel(Path("~/umms-RNAlabDATA/Software/B-PsiD_tools"
+                                 "/Zhang_HE_NatureProtocols_2023_SupplementaryTable1.xlsx").expanduser())
+
+      df = pd.merge(left, right, how = "left", on = "Motif")
+      df = df[df["Region"] == "3UTR"]
+      genome_coord = df[["Chrom", "GenomicModBase"]]
+
+      ## Split into two dataframes based on strand (+/-)
+      fwd_condition = genome_coord["Strand"] == "+"
+      fwd = genome_coord[fwd_condition]
+      rev = genome_coord[~fwd_condition]
+
+      ## Output as TSV files
+      base = "B-PsiD_UNUAR_motif_sites_mRNA_hg38"
+      types = ["fwd", "rev"]
+      unuar_sites = [fwd, rev]
+
+      for type, df in zip(types, unuar_sites):
+         output_name = current_path/f"{base}_{type}.tsv"
+         df.to_csv(output_name, sep = "\t", index = False)
+   
+   except Exception as e:
+      print("Failed to merge dataframes and output separate .TSVs based on strand: {e}")
+      traceback.print_exc()
+      raise
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = ("Merges dataframes for B-PsiD and splits "
+                                                    "based on strand [+/-] for UNUAR site"))
+    print("Splitting UNUAR .tsv file...")
+    main()
+    print("Process finished.")
