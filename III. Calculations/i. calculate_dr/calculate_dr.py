ec0fe2e26ca05dba83e58a948828bcdca008b1bf
Sonia <lingsn@umich.edu>
2026-09-01 16:23:28 -0400

    change comment

--- III. Calculations/i. calculate_dr/calculate_dr.py
+++ III. Calculations/i. calculate_dr/calculate_dr.py
@@ -108,7 +108,8 @@ class BaseDelCounter:
       """
       PURPOSE:
       1. Obtain all reads at specific genomic coordinate.
-      2. If read has no deletion before and/or after, append deletion count.
+      2. If read has 1D (single-nt deletion) in its CIGAR 
+         string, append deletion count.
          -> We only consider deletions of the central U in
             the UNUAR motif.
       3. Skip all reads that (i) don't have deletions or
