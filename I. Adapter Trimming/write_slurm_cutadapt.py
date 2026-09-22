832af20f20bc89a0152b3cc271d8caf9e215a813
Sonia <lingsn@umich.edu>
2026-09-22 15:07:26 -0400

    fix line formatting for sbatch

--- I. Adapter Trimming/write_slurm_cutadapt.py
+++ I. Adapter Trimming/write_slurm_cutadapt.py
@@ -6,7 +6,7 @@ import textwrap
 
 def main(input_folder: str, output_folder: str, email: str, 
          slurm_acct: str, walltime: str, mem: int):
-    '''
+    """
     PURPOSE:
     * Goes into folder containing all raw FASTQs and obtains all sample names.
     * Automatically appends new task containing sample name to SBATCH script,
@@ -20,7 +20,7 @@ def main(input_folder: str, output_folder: str, email: str,
     2. We append the following task to the SBATCH script:
        "python3 run_cutadapt_fastp.py --input raw_fastqs --output trimmed_reads \ 
         -C 2 -U 12 -S KEH-Rep1-7KO-HEK293T-Cyto-BS_S6"
-    '''
+    """
     current_path = Path.cwd()
     output = current_path/"SBATCHSubArr-CUT_FASTP.sbatch"
     
@@ -31,10 +31,10 @@ def main(input_folder: str, output_folder: str, email: str,
             "in your current working directory."
         )
     
-    '''
+    """
     1. Obtain all possible sample names
     2. Find total count, and subtract 1 so it's 0-based
-    '''
+    """
     sample_names = set([fastq.stem.split("_")[0] 
                         for fastq in start_dir.glob("*.fastq.gz")])
     num_jobs = len(sample_names) - 1
@@ -42,43 +42,45 @@ def main(input_folder: str, output_folder: str, email: str,
     ## Create SBATCH file if it doesn't exist
     if not output.exists():
         with open(output, "w") as f:
-            template_start = textwrap.dedent(f"""\
-                                            #!/usr/bin/env bash
-                                            #SBATCH --job-name=CUT_FASTP
-                                            #SBATCH --mail-user={email}
-                                            #SBATCH --mail-type=BEGIN,END,FAIL
-                                            #SBATCH --output=CUT_FASTP_%u_%A_%a.out
-                                            #SBATCH --array=0-{num_jobs}
-                                            #SBATCH --account={slurm_acct}
-                                            #SBATCH --time={walltime}
-                                            #SBATCH --mem={mem}m
-                                            #SBATCH --partition=standard
-                                            #SBATCH --ntasks-per-node=1
-                                            #SBATCH --nodes=1
-                                            ################################################################################
-                                            # Edit the strings under 'declare -a tasks=(' to match your experiments.
-                                            #
-                                            # The #SBATCH --array variable above creates an array [0,1,2,3]. Change it so that the length
-                                            # is how many jobs you need (same as number of strings under $tasks).
-                                            #
-                                            # This script is submitted that many times, but only one line from $tasks is
-                                            # evaluated each time.
-                                            #
-                                            # For more info on #SBATCH variables, see https://arc.umich.edu/greatlakes/slurm-user-guide/
-                                            # and https://slurm.schedmd.com/sbatch.html
-                                            #
-                                            # This requires a conda environment with samtools and pysam (B-PSID)
-                                            # 
-                                            # To call this script:
-                                            # sbatch write_slurm.sbatch
-                                            ################################################################################
+            template_start = textwrap.dedent(
+                f"""\
+                #!/usr/bin/env bash
+                #SBATCH --job-name=CUT_FASTP
+                #SBATCH --mail-user={email}
+                #SBATCH --mail-type=BEGIN,END,FAIL
+                #SBATCH --output=CUT_FASTP_%u_%A_%a.out
+                #SBATCH --array=0-{num_jobs}
+                #SBATCH --account={slurm_acct}
+                #SBATCH --time={walltime}
+                #SBATCH --mem={mem}m
+                #SBATCH --partition=standard
+                #SBATCH --ntasks-per-node=1
+                #SBATCH --nodes=1
+                ################################################################################
+                # Edit the strings under 'declare -a tasks=(' to match your experiments.
+                #
+                # The #SBATCH --array variable above creates an array [0,1,2,3]. Change it so that the length
+                # is how many jobs you need (same as number of strings under $tasks).
+                #
+                # This script is submitted that many times, but only one line from $tasks is
+                # evaluated each time.
+                #
+                # For more info on #SBATCH variables, see https://arc.umich.edu/greatlakes/slurm-user-guide/
+                # and https://slurm.schedmd.com/sbatch.html
+                #
+                # This requires a conda environment with samtools and pysam (B-PSID)
+                # 
+                # To call this script:
+                # sbatch write_slurm.sbatch
+                ################################################################################
 
-                                            module purge
-                                            eval "$(conda shell.bash hook)"
-                                            conda activate ~/miniconda3/envs/B-PSID
+                module purge
+                eval "$(conda shell.bash hook)"
+                conda activate ~/miniconda3/envs/B-PSID
 
-                                            declare -a tasks=(
-                                            """)
+                declare -a tasks=(
+                """
+            )
 
             f.write(template_start)
 
